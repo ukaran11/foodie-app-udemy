c4e0c4bcf8268cb802e96f20356b79ef36ed7fdd
utsav <ukaran11@gmail.com>
2025-01-05 14:43:06 +0530

    Utsav | Added images

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,12 +1,14 @@
-import MealItem from './meal-item';
-import classes from './meals-grid.module.css';
+import MealItem from "./meal-item";
+import classes from "./meals-grid.module.css";
 
 export default function MealsGrid({ meals }) {
   return (
     <u className={classes.meals}>
-      {meals.map((meal) => {
-        <li key={meal.id}><MealItem {...meal}/></li>
-      })}
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
+      ))}
     </u>
-  )
+  );
 }
