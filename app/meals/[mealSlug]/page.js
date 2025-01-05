1d2d3d3c350abc0e131498d5fcba3e04e850e1dd
utsav <ukaran11@gmail.com>
2025-01-06 03:22:01 +0530

    Utsav | Loading dynamic meal details

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,33 @@
-export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+import Image from 'next/image';
+import { getMeal } from '../../../lib/meals';
+import classes from './page.module.css';
+
+export default async function MealDetailsPage({params}) {
+  const {mealSlug} = await params;
+  const meal = getMeal(mealSlug)
+  console.log('meal', meal)
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+
+      <main>
+        <p className={classes.instructions}
+          dangerouslySetInnerHTML={{__html: meal.instructions}}>
+          </p>
+      </main>
+    </>
+  )
 }
+ 
