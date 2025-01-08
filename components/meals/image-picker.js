fa2b2e55735ae018643011dbfbbc7c8da99b9e61
utsav <ukaran11@gmail.com>
2025-01-08 20:25:16 +0530

    Utsav | Added image preview

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,28 +1,65 @@
-'use client'
-import { useRef } from 'react';
-import classes from './image-picker.module.css';
+"use client";
+import { useRef } from "react";
+import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
 
   function handlePickClick() {
     imageInput.current.click();
   }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.control}>
-        <input className={classes.input}
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+              />
+          )}
+        </div>
+        <input
+          className={classes.input}
           type="file"
           id={name}
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
-          />
-          <button onClick={handlePickClick} className={classes.button} type="button" >
-            Pick an Image
-          </button>
+          onChange={handleImageChange}
+          required
+        />
+        <button
+          onClick={handlePickClick}
+          className={classes.button}
+          type="button"
+        >
+          Pick an Image
+        </button>
       </div>
     </div>
-  )
+  );
 }
