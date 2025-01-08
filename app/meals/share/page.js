6a99a0c053949a5bfd92abd7817404fc90b5ace9
utsav <ukaran11@gmail.com>
2025-01-09 03:04:29 +0530

    Utsav | Managed form status with useFormStatus hook

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,4 +1,5 @@
 import ImagePicker from '../../../components/meals/image-picker';
+import MealsFormSubmit from '../../../components/meals/meals-form-submit';
 import { shareMeal } from '../../../lib/actions';
 import classes from './page.module.css';
 
@@ -42,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your Image" name="image"/>
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
